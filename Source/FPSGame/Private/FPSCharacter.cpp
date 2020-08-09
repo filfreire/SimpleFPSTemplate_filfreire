0685e2cfef53a39655912b92c0f6bc765a8a2d99
Filipe Freire <livrofubia@gmail.com>
2020-08-09 12:14:09 +0200

    network player view pitch

--- Source/FPSGame/Private/FPSCharacter.cpp
+++ Source/FPSGame/Private/FPSCharacter.cpp
@@ -48,6 +48,19 @@ void AFPSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompon
 	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
 }
 
+void AFPSCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (!IsLocallyControlled())
+	{
+		FRotator NewRot = CameraComponent->RelativeRotation;
+		NewRot.Pitch = (uint8) RemoteViewPitch * 360.0f / 255.0f;
+
+		CameraComponent->SetRelativeRotation(NewRot);
+	}
+}
+
 
 void AFPSCharacter::Fire()
 {
