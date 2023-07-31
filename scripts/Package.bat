121155a34632d4f87b742f1f8b91019a30affd15
Filipe Freire <livrofubia@gmail.com>
2023-07-31 22:34:29 +0100

    remove -prereqs

--- scripts/Package.bat
+++ scripts/Package.bat
@@ -4,4 +4,6 @@ set projectName=%~3
 set target=%~4
 set packageFolder=%~5
 
-"%ueLocation%\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun -project="%projectLocation%\%projectName%" -nop4 -utf8output -nocompileeditor -skipbuildeditor -cook -project="%projectLocation%\%projectName%" -target=%target% -platform=Win64 -installed -stage -archive -package -build -pak -iostore -compressed -prereqs -archivedirectory="%projectLocation%\%packageFolder%" -clientconfig=Development -nocompile -nocompileuat
+"%ueLocation%\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun -project="%projectLocation%\%projectName%" -nop4 -utf8output -nocompileeditor -skipbuildeditor -cook -project="%projectLocation%\%projectName%" -target=%target% -platform=Win64 -installed -stage -archive -package -build -pak -iostore -compressed -archivedirectory="%projectLocation%\%packageFolder%" -clientconfig=Development -nocompile -nocompileuat
+
+:: removed: -prereqs
--- scripts/PackageForShipping.bat
+++ scripts/PackageForShipping.bat
@@ -4,5 +4,6 @@ set projectName=%~3
 set target=%~4
 set packageFolder=%~5
 
-"%ueLocation%\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun -project="%projectLocation%\%projectName%" -nop4 -utf8output -nocompileeditor -skipbuildeditor -cook -project="%projectLocation%\%projectName%" -target=%target% -platform=Win64 -installed -stage -archive -package -build -pak -compressed -prereqs -archivedirectory="%projectLocation%\%packageFolder%" -clientconfig=Shipping -nocompile -nocompileuat -nodebuginfo
+"%ueLocation%\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun -project="%projectLocation%\%projectName%" -nop4 -utf8output -nocompileeditor -skipbuildeditor -cook -project="%projectLocation%\%projectName%" -target=%target% -platform=Win64 -installed -stage -archive -package -build -pak -compressed -archivedirectory="%projectLocation%\%packageFolder%" -clientconfig=Shipping -nocompile -nocompileuat -nodebuginfo
 
+:: removed: -prereqs
